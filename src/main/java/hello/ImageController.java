397fd5edfe2fea2c5ff8f244b0178b93f2794265
Gustavo Pacheco <ryctabo@gmail.com>
2017-11-29 14:45:21 -0500

    Binary message

--- src/main/java/hello/ImageController.java
+++ src/main/java/hello/ImageController.java
@@ -4,6 +4,7 @@ import org.apache.commons.codec.binary.Base64;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.BinaryMessage;
 
 import java.util.logging.Logger;
 
@@ -12,14 +13,17 @@ public class ImageController {
 
     private static final Logger LOGGER = Logger.getLogger(ImageController.class.getName());
 
+    private static final String FORMAT = "data:image/png;base64,%s";
+
     @MessageMapping("/image")
     @SendTo("topic/images")
-    public String greeting(String msg) {
-        LOGGER.info("Get image from bytes array.");
-        String response = Base64.encodeBase64String(msg.getBytes());
-        LOGGER.info("Convert image to Base64.");
-        LOGGER.info("image -> " + response);
-        return response;
+    public BinaryMessage greeting(String msg) {
+        LOGGER.info("Get image and convert in Base64.");
+        byte[] imageBase64 = Base64.encodeBase64(msg.getBytes());
+        LOGGER.info("Get response format.");
+        String response = String.format(FORMAT, new String(imageBase64));
+        LOGGER.info("Response: " + response.substring(0, 50));
+        return new BinaryMessage(response.getBytes());
     }
 
 }
