47a0d63b77493739ec972256f65a879c63184261
Gustavo Pacheco <ryctabo@gmail.com>
2017-11-29 17:01:00 -0500

    Ok, vale

--- src/main/java/hello/ImageController.java
+++ src/main/java/hello/ImageController.java
@@ -17,8 +17,14 @@ public class ImageController {
     @MessageMapping("/image")
     @SendTo("/topic/images")
     public ImageResponse greeting(String msg) {
-        LOGGER.info("Get image and convert in Base64.");
-        byte[] imageBase64 = Base64.encodeBase64(msg.getBytes());
+        LOGGER.info("Get image with this format: " + msg);
+        String[] byteValues = msg.substring(1, msg.length() - 1).split(",");
+        byte[] bytes = new byte[byteValues.length];
+
+        for (int i = 0; i < byteValues.length; i++)
+            bytes[i] = Byte.parseByte(byteValues[i].trim());
+
+        byte[] imageBase64 = Base64.encodeBase64(bytes);
         LOGGER.info("Get response format.");
         String response = String.format(FORMAT, new String(imageBase64));
         LOGGER.info("Response: " + response.substring(0, 50));
